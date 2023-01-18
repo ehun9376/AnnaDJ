a0c89575537af3df78be96ba63e64287e554306f
陳逸煌 <ehun9376@gmail.com>
2023-01-18 15:53:24 +0800

    no message

--- AnnaDJ/AppDelegate.swift
+++ AnnaDJ/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SKPaymentQueue.default().add(IAPCenter.shared)
         self.window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
         window?.makeKeyAndVisible()
